5782a3c71b5b3d804ad6f0332a4b8f43f94d89ca
Chasel Harold <155750018+TaYlaTan@users.noreply.github.com>
2024-03-09 10:34:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// 将金额转换为其他货币
+function convertCurrency(amount, fromCurrency, toCurrency) {
+  // 在这里实现货币转换
+}
+
+// 将金额格式化为货币格式
+function formatCurrency(amount, currency) {
+  // 在这里实现货币格式化
+}
+
+// 计算交易手续费
+function calculateTransactionFee(amount, feeRate) {
+  return amount * feeRate;
+}
